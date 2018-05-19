ee0754c9edbc90039ff8481dbb01de3afe5679f3
bwho2 <39171401+bwho2@users.noreply.github.com>
2018-05-19 13:54:07 -0400

    Update getElected.py

--- getElected.py
+++ getElected.py
@@ -68,10 +68,16 @@ def newCandidate():
     while correct = False:
         party = input("Your candidate's party: D or R? ")
         correct = True
-        regex = r'(^[Dd]((em)|(emocrat))?$)|(^[Rr]((ep)|(epublican))?$)'
+        regex = r'^([Dd]((em)|(emocrat))?)|([Rr]((ep)|(epublican))?)$'
         sekeer = re.compile(regex)
         if not(seeker.search(party).string == party): correct = False
-    gender = input("Your candidate's gender: M or F? ")
+    correct = False
+    while correct == False:
+        gender = input("Your candidate's gender: M, F, or O? ")
+        correct = True
+        regex = r'^(([Mm](ale)?)|([Ff](emale)?)|([Oo](ther)?))$'
+        seeker = re.compile(regex)
+        if seeker.search(gender) == None: correct = False
     if not(gender == 'M' or gender == 'F'):
         return 'Incorrect input'
     race = input ("Your candidate's race: White, Black, or Hispanic? ")
